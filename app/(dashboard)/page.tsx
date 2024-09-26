b93deac39e161b9c002cf7dccd8d2ad619107156
Chinmay-innovates <chinmaygope69@gmail.com>
2024-09-26 20:42:19 +0530

    fix search

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,22 +2,26 @@
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
+import { useSearchParams } from "next/navigation";
 
-interface DashboardPageProps {
-	searchParams: {
-		search?: string;
-		favourites?: string;
-	};
-}
+// interface DashboardPageProps {
+// 	searchParams: {
+// 		search?: string;
+// 		favourites?: string;
+// 	};
+// }
 
-export default function DashboardPage({ searchParams }: DashboardPageProps) {
+export default function DashboardPage() {
 	const { organization } = useOrganization();
+	const searchParams = useSearchParams();
+	const search = searchParams.get("search") || undefined;
+	const favourites = searchParams.get("favourites") || undefined;
 	return (
 		<div className="flex-1 h-[calc(100vh-80px)] p-6">
 			{!organization ? (
 				<EmptyOrg />
 			) : (
-				<BoardList orgId={organization.id} query={searchParams} />
+				<BoardList orgId={organization.id} query={{ search, favourites }} />
 			)}
 		</div>
 	);
